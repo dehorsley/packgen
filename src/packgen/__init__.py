381f0eeed4be129040339db806410cc6356e4ae8
David Horsley <david.e.horsley@gmail.com>
2025-05-17 21:06:14 +1000

    move to src layout

--- src/packgen/__init__.py
+++ src/packgen/__init__.py
@@ -1,9 +1,11 @@
 from packgen.packgen import (
     UnmarshalGenerator,
     JsonMarshalGenerator,
+    LengthGenerator,
 )
 
 __all__ = [
     "UnmarshalGenerator",
     "JsonMarshalGenerator",
+    "LengthGenerator",
 ]
